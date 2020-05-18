a1abff7cf889c55de3afbd17c07d01d9959f7202
Maitree Hirunteeyakun <h.mattjunk@gmail.com>
2020-05-18 15:42:44 +0700

    Search and summit

--- flask_myapp/routes/main.py
+++ flask_myapp/routes/main.py
@@ -6,6 +6,7 @@ import json
 import os
 import random
 import string
+import time
 
 #export FLASK_DEBUG=1
 #export FLASK_APP=localrun.py
@@ -19,3 +20,41 @@ main = Blueprint('main', __name__)
 @main.route('/',methods=['POST','GET'])
 def index():
     return "Welcome to CO2 Sensor Server"
+
+@main.route('/summit',methods=['POST','GET'])
+def summit():
+    cpuserial=request.args.get("serial")
+    timestamp=request.args.get("t")
+    CO2=float(request.args.get("co2"))
+    ts = time.time()
+    db.execute(f"""
+    INSERT INTO "Received Data" (cpuserial, timestamp, "Received timestamp", "CO2 ppm")
+    VALUES ('{cpuserial}', {timestamp}, {ts} ,{CO2});
+    COMMIT;
+    """)
+    return "success"
+
+@main.route('/search',methods=['POST','GET'])
+def search():
+    if (request.method == 'POST'):
+        cpuserial=request.args.get("serial")
+        if (cpuserial==None):
+            cpuserial=request.form.get("serial")
+        if (cpuserial==None or cpuserial==""):
+            return "ERROR getting Serial!"
+        fet=db.execute(f"""
+        SELECT timestamp,"CO2 ppm" FROM "Received Data" WHERE cpuserial='{cpuserial}'
+        """).fetchall()
+        csv="timestamp,CO2 ppm"
+        for i in fet:
+            csv=csv+"<br>"+str(i[0])+","+str(i[1])
+            print(i)
+        return csv
+    else:
+        return """
+            <label for="site-search">Search the CO2 sensor:</label>
+            <form action="/search" method="post">
+            <input type="search" id="site-search" name="serial" aria-label="Search through Database">
+            <button type="summit">Search</button>
+            </form>
+            """
